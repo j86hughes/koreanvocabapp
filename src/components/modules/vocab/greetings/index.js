102e6f287bc6bb697c24a4c9a3e78f48c20143ba
j86hughes <j86hughes@gmail.com>
2018-02-03 16:57:51 +0000

    Answer string trimmed. Multiple correct answers accepted for vocab

--- src/components/modules/vocab/greetings/index.js
+++ src/components/modules/vocab/greetings/index.js
@@ -1,43 +1,43 @@
 const wordsArray = [
   {
     korean: '안녕하세요',
-    english: 'hello',
+    english: ['hello'],
   },
   {
     korean: '안녕',
-    english: 'hi',
+    english: ['hi'],
   },
   {
     korean: '집',
-    english: 'house',
+    english: ['house'],
   },
   {
     korean: '물',
-    english: 'water',
+    english: ['water'],
   },
   {
     korean: '가깝다',
-    english: 'to be close',
+    english: ['to be close', 'close'],
   },
   {
     korean: '멀다',
-    english: 'to be far',
+    english: ['to be far', 'far'],
   },
   {
     korean: '길다',
-    english: 'to be long',
+    english: ['to be long', 'long'],
   },
   {
     korean: '짧다',
-    english: 'to be short',
+    english: ['to be short', 'short'],
   },
   {
     korean: '높다',
-    english: 'to be high',
+    english: ['to be high', 'high'],
   },
   {
     korean: '낮다',
-    english: 'to be low',
+    english: ['to be low', 'low'],
   },
 ]
 
