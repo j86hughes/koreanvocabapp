5dcf4b2697e2ae281124b33776bb3ad394ea650b
j86hughes <j86hughes@gmail.com>
2018-06-14 20:08:18 +0100

    added topik verbs 7

---
+++ src/components/modules/vocab/topikVerbs7/index.js
@@ -0,0 +1,334 @@
+import UUIDV4 from  'uuid/v4';
+
+export default [
+  //1
+  {
+    id: UUIDV4(),
+    korean: ['시작하다'],
+    english: ['to start', 'to begin (Something)'],
+  },
+  //2
+  {
+    id: UUIDV4(),
+    korean: ['시키다'],
+    english: ['to make (somebody do)', 'to order'],
+  },
+  //3
+  {
+    id: UUIDV4(),
+    korean: ['식사하다'],
+    english: ['to have a meal'],
+  },
+  //4
+  {
+    id: UUIDV4(),
+    korean: ['신다'],
+    english: ['to put on', 'to wear (on feet)'],
+  },
+  //5
+  {
+    id: UUIDV4(),
+    korean: ['싣다'],
+    english: ['to load'],
+  },
+  //6
+  {
+    id: UUIDV4(),
+    korean: ['실례하다'],
+    english: ['to annoy', 'to trouble'],
+  },
+  //7
+  {
+    id: UUIDV4(),
+    korean: ['실수하다'],
+    english: ['to make a mistake'],
+  },
+  //8
+  {
+    id: UUIDV4(),
+    korean: ['실패하다'],
+    english: ['to fail'],
+  },
+  //9
+  {
+    id: UUIDV4(),
+    korean: ['싫어하다'],
+    english: ['to hate', 'to dislike'],
+  },
+  //10
+  {
+    id: UUIDV4(),
+    korean: ['심다'],
+    english: ['to plant'],
+  },
+  //11
+  {
+    id: UUIDV4(),
+    korean: ['싸다'],
+    english: ['to wrap', 'to pack'],
+  },
+  //12
+  {
+    id: UUIDV4(),
+    korean: ['싸우다'],
+    english: ['to fight', 'to argue'],
+  },
+  //13
+  {
+    id: UUIDV4(),
+    korean: ['쌓다'],
+    english: ['to pile, to stack', 'to accumulate'],
+  },
+  //14
+  {
+    id: UUIDV4(),
+    korean: ['쌓이다'],
+    english: ['to be stacked', 'to pile up'],
+  },
+  //15
+  {
+    id: UUIDV4(),
+    korean: ['썰다'],
+    english: ['to cut', 'to chop', 'to slice'],
+  },
+  //16
+  {
+    id: UUIDV4(),
+    korean: ['쏟아지다'],
+    english: ['to pour'],
+  },
+  //17
+  {
+    id: UUIDV4(),
+    korean: ['쓰다'],
+    english: ['to write'],
+  },
+  {
+    id: UUIDV4(),
+    korean: ['쓰다'],
+    english: ['to wear, to put on (on or overhead)'],
+  },
+  {
+    id: UUIDV4(),
+    korean: ['쓰다'],
+    english: ['to use, to spend'],
+  },
+  //18
+  {
+    id: UUIDV4(),
+    korean: ['쓰이다'],
+    english: ['to be used'],
+  },
+  //19
+  {
+    id: UUIDV4(),
+    korean: ['씹다'],
+    english: ['to chew', 'to bite'],
+  },
+  //20
+  {
+    id: UUIDV4(),
+    korean: ['씻다'],
+    english: ['to wash'],
+  },
+  //21
+  {
+    id: UUIDV4(),
+    korean: ['아끼다'],
+    english: ['to save'],
+  },
+  {
+    id: UUIDV4(),
+    korean: ['아끼다'],
+    english: ['to cherish'],
+  },
+  //22
+  {
+    id: UUIDV4(),
+    korean: ['안다'],
+    english: ['to hug', 'to hold'],
+  },
+  //23
+  {
+    id: UUIDV4(),
+    korean: ['안되다'],
+    english: ['to be unsuccessful'],
+  },
+  //24
+  {
+    id: UUIDV4(),
+    korean: ['앉다'],
+    english: ['to sit'],
+  },
+  //25
+  {
+    id: UUIDV4(),
+    korean: ['않다'],
+    english: ['to be not', 'to not be', 'not to be'],
+  },
+  //26
+  {
+    id: UUIDV4(),
+    korean: ['알다'],
+    english: ['to know'],
+  },
+  //27
+  {
+    id: UUIDV4(),
+    korean: ['알리다'],
+    english: ['to let somebody know', 'to inform'],
+  },
+  //28
+  {
+    id: UUIDV4(),
+    korean: ['알아보다'],
+    english: ['to check', 'to investigate'],
+  },
+  {
+    id: UUIDV4(),
+    korean: ['알아보다'],
+    english: ['to recognize'],
+  },
+  //29
+  {
+    id: UUIDV4(),
+    korean: ['앓다'],
+    english: ['to suffer (from)', 'to be sick'],
+  },
+  //30
+  {
+    id: UUIDV4(),
+    korean: ['애쓰다'],
+    english: ['to try', 'to endeavour'],
+  },
+  //31
+  {
+    id: UUIDV4(),
+    korean: ['약속하다'],
+    english: ['to promise'],
+  },
+  //32
+  {
+    id: UUIDV4(),
+    korean: ['어울리다'],
+    english: ['to get along'],
+  },
+  {
+    id: UUIDV4(),
+    korean: ['어울리다'],
+    english: ['to match', 'to suit'],
+  },
+  //33
+  {
+    id: UUIDV4(),
+    korean: ['얻다'],
+    english: ['to borrow'],
+  },
+  {
+    id: UUIDV4(),
+    korean: ['얻다'],
+    english: ['to gain', 'to get'],
+  },
+  //34
+  {
+    id: UUIDV4(),
+    korean: ['없애다'],
+    english: ['to remove', 'to get rid of'],
+  },
+  //35
+  {
+    id: UUIDV4(),
+    korean: ['여행하다'],
+    english: ['to travel'],
+  },
+  //36
+  {
+    id: UUIDV4(),
+    korean: ['연구하다'],
+    english: ['to study', 'to research'],
+  },
+  //37
+  {
+    id: UUIDV4(),
+    korean: ['연습하다'],
+    english: ['to practice'],
+  },
+  //38
+  {
+    id: UUIDV4(),
+    korean: ['열다'],
+    english: ['to open'],
+  },
+  //39
+  {
+    id: UUIDV4(),
+    korean: ['열리다'],
+    english: ['to open', 'to be opened'],
+  },
+  //40
+  {
+    id: UUIDV4(),
+    korean: ['오다'],
+    english: ['to come'],
+  },
+  //41
+  {
+    id: UUIDV4(),
+    korean: ['오르다'],
+    english: ['to rise', 'to go up'],
+  },
+  //42
+  {
+    id: UUIDV4(),
+    korean: ['올라가다'],
+    english: ['to go up', 'to rise'],
+  },
+  //43
+  {
+    id: UUIDV4(),
+    korean: ['올라오다'],
+    english: ['to come up'],
+  },
+  //44
+  {
+    id: UUIDV4(),
+    korean: ['올리다'],
+    english: ['to raise', 'to lift', 'to post'],
+  },
+  //45
+  {
+    id: UUIDV4(),
+    korean: ['옮기다'],
+    english: ['to move', 'to shift'],
+  },
+  //46
+  {
+    id: UUIDV4(),
+    korean: ['외치다'],
+    english: ['to shout', 'to yell'],
+  },
+  //47
+  {
+    id: UUIDV4(),
+    korean: ['요구하다'],
+    english: ['to request', 'to demand'],
+  },
+  //48
+  {
+    id: UUIDV4(),
+    korean: ['요리하다'],
+    english: ['to cook'],
+  },
+  //49
+  {
+    id: UUIDV4(),
+    korean: ['운동하다'],
+    english: ['to do exercise'],
+  },
+  //50
+  {
+    id: UUIDV4(),
+    korean: ['운전하다'],
+    english: ['to drive'],
+  },
+]
