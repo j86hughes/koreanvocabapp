eca361a11b7c1bc25c15402cb11d276ab64a02b4
j86hughes <j86hughes@gmail.com>
2019-04-13 14:07:26 +0100

    removed useless page files

--- src/components/pages/questionSection/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { QuestionPage } from '../../../redux/modules/superReducer/container';
-
-const QuestionSectionPage = ({ history }) => (
-  <QuestionPage history={history}/>
-)
-
-export default QuestionSectionPage;
